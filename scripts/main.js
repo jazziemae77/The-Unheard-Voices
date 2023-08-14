39dea76f72ad07d3393ff7a2a7c4ddb3acbe08e0
JazzleDazzles <72368428+JazzleDazzles@users.noreply.github.com>
2023-08-14 06:22:00 +0000

    finished nav
    finished nav

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
 var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function(){
-  document.querySelector("body").classList.toggle("active");
-})
+hamburger.addEventListener("click", function() {
+  var bodyElement = document.querySelector("body");
+  
+  // Toggle the 'active' class on the body element
+  bodyElement.classList.toggle("active");
+
+  // Add or remove the 'inactive' class based on the active state
+  if (bodyElement.classList.contains("active")) {
+    bodyElement.classList.remove("inactive");
+  } else {
+    bodyElement.classList.add("inactive");
+  }
+});
