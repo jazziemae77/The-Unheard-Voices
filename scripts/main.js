a38df8ed701b4202820539dcc3e8aa2a35da00e2
JazzleDazzles <72368428+JazzleDazzles@users.noreply.github.com>
2023-08-22 04:13:34 +0000

    edited about page with placeholder text and designs

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,37 @@ hamburger.addEventListener("click", function() {
     bodyElement.classList.add("inactive");
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
+
+  function showSlide(index) {
+    slides[currentSlide].classList.remove("active-slide");
+    slides[index].classList.add("active-slide");
+    currentSlide = index;
+  }
+
+const prevButton = document.createElement("button");
+prevButton.innerHTML = '<i class="fas fa-chevron-left"></i>';
+prevButton.classList.add("nav-button", "prev-button");
+prevButton.addEventListener("click", () => {
+  const prevSlide = (currentSlide - 1 + slides.length) % slides.length;
+  showSlide(prevSlide);
+});
+
+const nextButton = document.createElement("button");
+nextButton.innerHTML = '<i class="fas fa-chevron-right"></i>';
+nextButton.classList.add("nav-button", "next-button");
+nextButton.addEventListener("click", () => {
+  const nextSlide = (currentSlide + 1) % slides.length;
+  showSlide(nextSlide);
+});
+
+const carousel = document.querySelector(".slideshow");
+carousel.appendChild(prevButton);
+carousel.appendChild(nextButton);
+
+showSlide(currentSlide);
+});
+
