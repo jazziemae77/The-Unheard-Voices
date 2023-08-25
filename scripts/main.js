a2627c25c303f3a94d6b30119897654205e65bbd
jazziemae77 <72368428+jazziemae77@users.noreply.github.com>
2023-08-25 19:18:36 +0000

    redesigned navbar and sidebar for mobile devices (2)

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,16 @@
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function() {
+  var hamburger = document.querySelector(".hamburger");
   var bodyElement = document.querySelector("body");
   
-  // Toggle the 'active' class on the body element
-  bodyElement.classList.toggle("active");
+  hamburger.addEventListener("click", function() {
+    // Toggle the 'active' class on the body element
+    bodyElement.classList.toggle("active");
 
-  // Add or remove the 'inactive' class based on the active state
-  if (bodyElement.classList.contains("active")) {
-    bodyElement.classList.remove("inactive");
-  } else {
-    bodyElement.classList.add("inactive");
-  }
+    // Add or remove the 'inactive' class based on the active state
+    if (bodyElement.classList.contains("active")) {
+      bodyElement.classList.remove("inactive");
+    } else {
+      bodyElement.classList.add("inactive");
+    }
+  });
 });
-
